4848345ba3d7c998fc0ce507b6c1da273e5dd1a8
R.Yamaji <yamayamayamaji+github@gmail.com>
2012-06-06 21:51:54 +0900

    0.0.1 -> 0.0.2

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ youRoomをちょっとだけ快適にします。
 youRoom (https://www.youroom.in/ )のヘルパー chrome extension です。  
   
 #### 機能：  
-1. Facebookガジェットを非表示にします。  
+1. Facebookガジェットが非表示になります。(0.0.1-)  
+2. Shift + Enter で投稿できるようになります。(0.0.2-)  
+3. ミーティング(チャット)機能を使用できます。(0.0.2-)  
   
   
 #### 使用方法：  
--- youRoomHelper/content.js
+++ youRoomHelper/content.js
@@ -14,6 +14,7 @@ $CS = {
 };
 
 STORE_KEY = {
+	MEETING_MODE: 'meeting_mode',
 	PREV_VERSION: 'prev_version',
 	VERSION_CONFIRMED: 'version_confirmed'
 };
@@ -23,8 +24,21 @@ youRoomHelper = {
 	 * 初期処理
 	 */
 	init: function(){
+		this.retryMgr.init();
+
 		//Facebookガジェットを非表示にする
 		$('.social-gadget').remove();
+
+		//Shift + Enter で投稿できるようにする
+		$('.entry_content').live('keydown', function(e){
+			if (e.keyCode == 13 && e.shiftKey) {
+				$(this).closest('form').find('input:submit').get(0).click();
+				e.preventDefault();
+			}
+		});
+
+		//ミーティングモードを使用できるようにする
+		this.meetingModeMgr.init();
 
 		//バージョン更新確認
 		if (sessionStorage[STORE_KEY.VERSION_CONFIRMED] != 'true') {
@@ -33,7 +47,151 @@ youRoomHelper = {
 	},
 
 	/**
-	 * バージョン管理オブジェクト
+	 * ミーティングモード管理
+	 */
+	meetingModeMgr: {
+		INTERVAL: 3000,
+		entryQueue: [],
+		lastEntryTimestamp: '',
+		//初期化
+		init: function(){
+			var matches;
+
+			//ミーティングモード用移動ボタンを表示
+			//(ホーム画面)
+			if (location.pathname.match(/^\/*$/)) {
+				//ホーム画面では読み込みを待ちながらリトライする
+				youRoomHelper.retryMgr.reg(
+					function(){ return $('.entry-container').length; },
+					this.addMeetingBtn, this);
+			} else {
+				this.addMeetingBtn();
+			}
+
+			//ミーティングモード起動条件が揃っていればミーティングを起動
+			if (sessionStorage[STORE_KEY.MEETING_MODE] == 'on' &&
+					(matches = location.pathname.match(/r\/([^\/]+)\/entries\/([^\/]+)/))) {
+				this.roomId = matches[1];
+				this.entryId = matches[2];
+				this.boot();
+			} else {
+				this.end();
+			}
+		},
+
+		//ミーティングモード用移動ボタンを追加
+		addMeetingBtn: function(){
+			var imgUrl = chrome.extension.getURL('/images/meeting.png');
+			$('.entry-container').find('.topic-edit-actions').each(function(){
+				var $this = $(this), c;
+				c = $this.find('.btn-edit').closest('li').clone();
+				c.find('a')
+					.removeClass('edit_comment')
+					.css({backgroundImage: 'url(' + imgUrl + ')'})
+					.find('span').text('ミーティング')
+					.click(function(){
+						sessionStorage[STORE_KEY.MEETING_MODE] = 'on';
+					});
+				c.appendTo($this);
+			});
+		},
+
+		//ミーティングモード起動
+		boot: function(){
+			//cssクラス追加
+			var ss = $('<style type="text/css">').appendTo('head').get(0).sheet;
+			ss.insertRule('@-webkit-keyframes fadeOut {0% {opacity: 1;} 100% {opacity: 0;}}', 0);
+			ss.insertRule('.fadeOut {-webkit-animation-name: fadeOut; -webkit-animation-fill-mode: both; -webkit-animation-duration: 3s; -webkit-animation-delay: 30s;}', 1);
+
+			this.getNewEntries();
+		},
+
+		//ミーティングモード終了
+		end: function(){
+			if (this.timer) { clearTimeout(this.timer); }
+			sessionStorage.removeItem[STORE_KEY.MEETING_MODE];
+		},
+
+		//新規エントリーを取得
+		getNewEntries: function(){
+			$.getJSON('/r/' + this.roomId,
+				{
+					since: this.lastEntryTimestamp,
+					read_state:'',
+					flat: 'true'
+				},
+				$.proxy(this.handleNewEntries, this)
+			);
+			this.timer = setTimeout($.proxy(this.getNewEntries, this), this.INTERVAL);
+		},
+
+		//新規エントリー取得コールバック
+		handleNewEntries: function(newEntries){
+			var i, entry, entryObj;
+			for (i=0; entryObj=newEntries[i++];) {
+				entry = entryObj.entry;
+				//取得したエントリーの中で最終のもののタイムスタンプを記憶
+				if (entry.updated_at > this.lastEntryTimestamp) {
+					this.lastEntryTimestamp = entry.updated_at;
+				}
+				//既に表示されているエントリーまたは別ルームのエントリーはスルー
+				if ($('#entry_' + entry.id).length || entry.root_id != this.entryId) {
+					continue;
+				} else {
+					this.entryQueue.push(entry);
+				}
+			}
+			//表示対象となるエントリーがあれば表示する
+			if (this.entryQueue) {
+				//古い順に並び替え
+				this.entryQueue.sort(function(e1, e2){
+					var p = e1.updated_at,
+						q = e2.updated_at;
+					return (p > q) ? 1 : (p == q) ? 0 : -1;
+				});
+				this.showEntries();
+			}
+		},
+
+		//エントリーを表示
+		showEntries: function(){
+			if (!this.entryQueue.length) { return; }
+			//現在のルーム・エントリーのhtmlを取得
+			$.get('/r/' + this.roomId + '/entries/' + this.entryId + '/',
+				$.proxy(function(res){
+					var entry;
+					res = res.replace(/<(script|style|title)[^<]+<\/(script|style|title)>/gm,'').replace(/<(link|meta)[^>]+>/g,'');
+					this.$entryDom = $(res)/*.find('#contents-container')*/.find('#column1');
+					while (entry = this.entryQueue[0]) {
+						if (this.$entryDom.find('#entry_' + entry.id).length) {
+							this.entryQueue.shift();
+							this.showEntry(entry);
+						}
+						this.markAsUnread(entry);
+					}
+				}, this),
+				'html'
+			);
+		},
+
+		//エントリーを表示(1件毎)
+		showEntry: function(entry){
+			var containerSelector = '.comment-container';
+			var $newContainer = this.$entryDom.find('#entry_' + entry.id).closest(containerSelector);
+			var $oldContainer = $('#' + $newContainer.find('.comment-wrapper-lv1[id^=entry]').attr('id')).closest(containerSelector);
+			$oldContainer.remove();
+			$newContainer.hide().insertBefore($(containerSelector + ':last')).fadeIn(2500);
+		},
+
+		//エントリーに未読マークを表示
+		markAsUnread: function(entry){
+			var c = $('#entry_' + entry.id).find('.comment-parag .clearfix');
+			$('<span class="unread-comment-dot">').addClass('fadeOut').prependTo(c);
+		},
+	},
+
+	/**
+	 * バージョン管理
 	 */
 	versionMgr: {
 		//初期化
@@ -69,6 +227,49 @@ youRoomHelper = {
 				padding: '10px',
 				textAlign: 'left'
 			}).appendTo($('#column3') || $(document.body || document.frames[0]));
+		}
+	},
+
+	/**
+	 * リトライ管理
+	 */
+	retryMgr: {
+		//初期化
+		init: function(){
+			this.key = 1;
+			this.cnt = {};
+			this.max = { def: 10 };
+		},
+		//リトライ回数カウントアップ
+		countUp:  function(key){
+			return (++this.cnt[key] || (this.cnt[key] = 1));
+		},
+		//最大リトライ回数に達していないか
+		unreachedToMax: function(key){
+			return (this.cnt[key] || 0) < (this.max[key] || this.max.def);
+		},
+		//リトライ対象として登録する
+		reg: function(condition, fnc, context){
+			this.retry(this.key++, condition, fnc, context);
+		},
+		//リトライ実行
+		retry: function(key, condition, fnc, context){
+			var judgment;
+			try {
+				judgment = $.isFunction(condition) ? condition() : eval(condition);
+			} catch (e) { judgment = false; }
+
+			if (judgment) {
+				fnc.call(context);
+			} else {
+				if (this.unreachedToMax(key)) {
+					this.countUp(key);
+					setTimeout($.proxy(function(){
+						return this.retry(key, condition, fnc, context);
+					}, this), 300);
+				}
+				return;
+			}
 		}
 	}
 };
